b1de79e521191f02c23c02d23e219b7725dd1b08
foreska <43631784+foreska@users.noreply.github.com>
2019-01-29 00:38:45 +0100

    Update create-test-incoming-phone-numbers-example-1.3.x.js

--- api/v2010/incoming_phone_number/create-test-incoming-phone-numbers-example-1/create-test-incoming-phone-numbers-example-1.3.x.js
+++ api/v2010/incoming_phone_number/create-test-incoming-phone-numbers-example-1/create-test-incoming-phone-numbers-example-1.3.x.js
@@ -1,13 +1,13 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
-const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const accountSid = 'AC758e4fa2554b0dbfd9337936383f42f9';
 const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
 client.incomingPhoneNumbers
       .create({
-         phoneNumber: '+15005550006',
-         voiceUrl: 'http://demo.twilio.com/docs/voice.xml'
+         phoneNumber: '+18738002038',
+         voiceUrl: 'http://twilio.com/docs/voice.xml'
        })
       .then(incoming_phone_number => console.log(incoming_phone_number.sid))
       .done();
